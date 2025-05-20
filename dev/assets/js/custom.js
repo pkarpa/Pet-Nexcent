cd6a419a42fb925089fec8e76bfa899c39a0bb43
pKarpa <pavlo.karpa17@gmail.com>
2025-05-20 12:15:15 +0300

    feat: create slider desktop and mobile

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,15 @@
+  const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
   
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+
+})
